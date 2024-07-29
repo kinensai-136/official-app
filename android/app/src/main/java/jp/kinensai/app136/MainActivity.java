2d335a079aabc0fde57b717bc0951cbfb3562f61
captiino <169031030+captiino@users.noreply.github.com>
2024-07-29 21:11:57 +0900

    Capacitorを初期化

---
+++ android/app/src/main/java/jp/kinensai/app136/MainActivity.java
@@ -0,0 +1,5 @@
+package jp.kinensai.app136;
+
+import com.getcapacitor.BridgeActivity;
+
+public class MainActivity extends BridgeActivity {}
---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'jp.kinensai.app136',
+  appName: 'official-app',
+  webDir: 'build/client'
+};
+
+export default config;
