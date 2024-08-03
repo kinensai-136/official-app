566810edd3bdc89d69f5c60ed191429aeba8371f
captiino <169031030+captiino@users.noreply.github.com>
2024-08-03 15:29:59 +0900

    背景色を設定

--- app/root.tsx
+++ app/root.tsx
@@ -22,7 +22,7 @@ export function Layout({ children }: { children: React.ReactNode }) {
         <Meta />
         <Links />
       </head>
-      <body>
+      <body className="bg-dark-100">
         {children}
         <ScrollRestoration />
         <Scripts />
