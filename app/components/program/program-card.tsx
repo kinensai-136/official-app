35679d830be6c4a21c7b21f851e4c7839b0028a3
captiino <169031030+captiino@users.noreply.github.com>
2024-08-13 20:23:52 +0900

    企画カードからドロワーを開けるように

--- app/components/program/program-card.tsx
+++ app/components/program/program-card.tsx
@@ -1,3 +1,4 @@
+import { ProgramDrawer } from "~/components/program/program-drawer"
 import { Program, ProgramCategory } from "~/services/program/program.type"
 
 const categoriesMap = {
@@ -13,26 +14,30 @@ type Props = {
 
 export function ProgramCard({ program }: Props) {
   return (
-    <div className="flex items-end justify-between gap-2 rounded bg-dark-200 px-5 py-4">
-      <div className="flex-1 space-y-2 overflow-hidden">
-        <div>
-          <p className="text-sm text-dark-600">{program.organizer}</p>
-          <h1 className="truncate text-lg font-semibold text-white">
-            {program.title}
-          </h1>
+    <ProgramDrawer program={program}>
+      <div className="flex items-end justify-between gap-2 rounded bg-dark-200 px-5 py-4">
+        <div className="flex-1 space-y-2 overflow-hidden">
+          <div>
+            <p className="text-sm text-dark-600">{program.organizer}</p>
+            <h1 className="truncate text-lg font-semibold text-white">
+              {program.title}
+            </h1>
+          </div>
+          <ul className="flex flex-wrap gap-1 [scrollbar-width:none]">
+            {...program.tags.map((tag) => (
+              <li
+                key={tag}
+                className="inline-block whitespace-nowrap rounded-full bg-dark-300 px-2.5 py-px text-sm font-medium text-primary-500"
+              >
+                {tag}
+              </li>
+            ))}
+          </ul>
         </div>
-        <ul className="flex flex-wrap gap-1 [scrollbar-width:none]">
-          {...program.tags.map((tag) => (
-            <li
-              key={tag}
-              className="inline-block whitespace-nowrap rounded-full bg-dark-300 px-2.5 py-px text-sm font-medium text-primary-500"
-            >
-              {tag}
-            </li>
-          ))}
-        </ul>
+        <p className="text-sm text-dark-500">
+          {categoriesMap[program.category]}
+        </p>
       </div>
-      <p className="text-sm text-dark-500">{categoriesMap[program.category]}</p>
-    </div>
+    </ProgramDrawer>
   )
 }
