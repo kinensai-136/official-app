0c6b151a227ba606c76fe953c60357dfa4ad1e63
captiino <169031030+captiino@users.noreply.github.com>
2024-08-19 00:26:12 +0900

    各コンポーネントにてお気に入りボタンが利用可能に

--- app/components/program/program-drawer.tsx
+++ app/components/program/program-drawer.tsx
@@ -9,6 +9,7 @@ import { Location } from "~/components/program/drawer-parts/location"
 import { Schedule } from "~/components/program/drawer-parts/schedule"
 import { Tags } from "~/components/program/drawer-parts/tags"
 import { TimeTable } from "~/components/program/drawer-parts/time-table"
+import { FavoriteButton } from "~/components/program/favorite-button"
 import { OpenMapButton } from "~/components/program/open-map-button"
 import { Program, ProgramCategory } from "~/services/program/program.type"
 
@@ -39,11 +40,15 @@ export function ProgramDrawer({ program, children }: Props) {
         <Drawer.Content className="fixed inset-x-0 bottom-0 z-40 flex h-4/5 flex-col border-t border-dark-300 bg-dark-200 px-6 py-4 pb-16 text-white focus:outline-none">
           <div className="mx-auto mb-5 h-1 w-16 rounded-full bg-dark-400" />
           <div className="flex-1 space-y-3">
-            <div>
-              <p className="leading-none text-dark-600">{program.organizer}</p>
-              <p className="text-2xl font-bold">{program.title}</p>
+            <div className="flex justify-between gap-5">
+              <div>
+                <p className="leading-none text-dark-600">
+                  {program.organizer}
+                </p>
+                <p className="text-2xl font-bold">{program.title}</p>
+              </div>
+              <FavoriteButton program={program} size="large" />
             </div>
-
             <Tags>{program.tags}</Tags>
             <div className="flex gap-8">
               <Location>{program.location}</Location>
