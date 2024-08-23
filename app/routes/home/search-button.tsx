431bc44b8a1c6f87ef8d4c81904766653b502e54
captiino <169031030+captiino@users.noreply.github.com>
2024-08-23 14:45:17 +0900

    ホーム画面の「模擬店」の色を変更

--- app/routes/home/search-button.tsx
+++ app/routes/home/search-button.tsx
@@ -5,13 +5,13 @@ export function SearchButton() {
   return (
     <Link
       to="/home/search"
-      className="flex items-center gap-4 rounded-full border border-dark-300 bg-white/5 p-1 text-dark-600"
+      className="flex items-center gap-4 rounded-full border border-dark-300 bg-white/5 p-1.5 text-dark-600"
     >
       <MagnifyingGlassIcon className="ml-5 size-6 shrink-0" />
       <p className="mr-auto">企画を検索する</p>
       <Link
         to="/map/booth"
-        className="flex items-center gap-2 rounded-full bg-dark-300 px-6 py-2.5 text-primary-400"
+        className="flex items-center gap-2 rounded-full bg-primary-100 px-6 py-2 text-white"
       >
         <ShoppingBagIcon className="size-6" />
         <p className="font-medium">模擬店</p>
