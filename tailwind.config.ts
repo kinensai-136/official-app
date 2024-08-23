2ab13e7fa498c58145371fc56fdb899b11678398
captiino <169031030+captiino@users.noreply.github.com>
2024-08-23 11:10:47 +0900

    トーストのアニメーションが欠落していた問題を修正

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -40,6 +40,34 @@ export default {
       lg: "1.5rem",
       full: "9999px",
     },
+    extend: {
+      animation: {
+        enter: "enter .2s ease-out",
+        leave: "leave .15s ease-in forwards",
+      },
+      keyframes: {
+        enter: {
+          "0%": {
+            opacity: "0",
+            transform: "scale(.9)",
+          },
+          "100%": {
+            opacity: "1",
+            transform: "scale(1)",
+          },
+        },
+        leave: {
+          "0%": {
+            opacity: "1",
+            transform: "scale(1)",
+          },
+          "100%": {
+            opacity: "0",
+            transform: "scale(.9)",
+          },
+        },
+      },
+    },
   },
   plugins: [],
 } satisfies Config
