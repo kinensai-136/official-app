5eb3aeab2c604ae6fca8247c0477b20e1cc410a2
captiino <169031030+captiino@users.noreply.github.com>
2024-09-16 03:35:08 +0900

    ホーム画面「お気に入り」がゼロの時の表示を追加

--- app/routes/home/sections/favorite-section.tsx
+++ app/routes/home/sections/favorite-section.tsx
@@ -1,3 +1,5 @@
+import { Link } from "@remix-run/react"
+
 import { ProgramCard } from "~/components/program/program-card"
 import { useFavorite } from "~/services/favorite/favorite-hook"
 
@@ -10,11 +12,22 @@ export function FavoriteSection() {
         <p className="text-dark-600">{favoritePrograms.length}つを追加済み</p>
       </div>
       <ul className="-mb-3 flex gap-2 overflow-scroll pb-3">
-        {favoritePrograms.map((program) => (
-          <li key={program._id} className="w-56 min-w-56">
-            <ProgramCard program={program} />
-          </li>
-        ))}
+        {favoritePrograms.length > 0 ? (
+          favoritePrograms.map((program) => (
+            <li key={program._id} className="w-56 min-w-56">
+              <ProgramCard program={program} />
+            </li>
+          ))
+        ) : (
+          <p className="w-full p-4 text-center text-dark-400">
+            まだお気に入りに何もありません
+            <br />
+            <Link to="/home/search" className="text-primary-300">
+              企画を探し
+            </Link>
+            てみませんか？
+          </p>
+        )}
       </ul>
     </section>
   )
