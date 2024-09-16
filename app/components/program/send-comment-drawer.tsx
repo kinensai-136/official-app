ef69b730fa44332453f821c8ae3f622d411d5ffe
captiino <169031030+captiino@users.noreply.github.com>
2024-09-17 00:55:00 +0900

    感想コメントの注意事項を変更

--- app/components/program/send-comment-drawer.tsx
+++ app/components/program/send-comment-drawer.tsx
@@ -65,7 +65,7 @@ export function SendCommentDrawer({ program, children }: Props) {
               className="w-full rounded-sm border border-primary-300 bg-dark-300 px-4 py-3 focus:outline-none"
             />
             <Information>
-              後日一部のコメントが主催者へ匿名で伝えられます。それまでは「データの消去」から送信が取り消せます。
+              一部の企画を除き、後日主催者へ匿名で伝えられます。良識あるコメントを心がけましょう。
             </Information>
             <Field className="flex items-center gap-2 px-px">
               <Checkbox
