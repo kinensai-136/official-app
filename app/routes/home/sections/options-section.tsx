a2325a60873b902ad5ea20596a73e8c5eb57a7a8
captiino <169031030+captiino@users.noreply.github.com>
2024-09-22 11:16:46 +0900

    XとInstagramのボタンも追加

--- app/routes/home/sections/options-section.tsx
+++ app/routes/home/sections/options-section.tsx
@@ -1,7 +1,5 @@
 import { Browser } from "@capacitor/browser"
-
-const buttonStyle =
-  "rounded-[0.8rem] text-center bg-dark-200 p-2.5 text-dark-600"
+import { ArrowTopRightOnSquareIcon } from "@heroicons/react/24/solid"
 
 export function OptionsSection() {
   return (
@@ -9,6 +7,8 @@ export function OptionsSection() {
       <h2 className="px-0.5 text-xl font-medium text-white">各種オプション</h2>
       <div className="grid grid-cols-2 gap-1">
         <OfficialHPButton />
+        <OfficialTwitterXButton />
+        <OfficialInstagramButton />
       </div>
     </section>
   )
@@ -17,8 +17,39 @@ export function OptionsSection() {
 function OfficialHPButton() {
   const handleClick = () => Browser.open({ url: "https://kinensai.jp" })
   return (
-    <button onClick={handleClick} className={buttonStyle}>
-      公式HP
+    <button
+      onClick={handleClick}
+      className="flex items-center justify-between rounded-[0.8rem] bg-dark-200 px-5 py-2.5 text-dark-600"
+    >
+      ホームページ
+      <ArrowTopRightOnSquareIcon className="size-5 text-dark-400" />
+    </button>
+  )
+}
+
+function OfficialTwitterXButton() {
+  const handleClick = () => Browser.open({ url: "https://x.com/kinensai" })
+  return (
+    <button
+      onClick={handleClick}
+      className="flex items-center justify-between rounded-[0.8rem] bg-dark-200 px-5 py-2.5 text-dark-600"
+    >
+      X(旧Twitter)
+      <ArrowTopRightOnSquareIcon className="size-5 text-dark-400" />
+    </button>
+  )
+}
+
+function OfficialInstagramButton() {
+  const handleClick = () =>
+    Browser.open({ url: "https://instagram.com/tokaikinensai" })
+  return (
+    <button
+      onClick={handleClick}
+      className="flex items-center justify-between rounded-[0.8rem] bg-dark-200 px-5 py-2.5 text-dark-600"
+    >
+      Instagram
+      <ArrowTopRightOnSquareIcon className="size-5 text-dark-400" />
     </button>
   )
 }
