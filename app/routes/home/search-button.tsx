819550eb4f3500a0130f34f61c61d1dce167ab79
captiino <169031030+captiino@users.noreply.github.com>
2024-09-25 20:31:56 +0900

    ホーム画面「検索バー」が不自然に改行する問題を修正

--- app/routes/home/search-button.tsx
+++ app/routes/home/search-button.tsx
@@ -5,16 +5,16 @@ export function SearchButton() {
   return (
     <Link
       to="/home/search"
-      className="flex items-center gap-4 rounded-full border border-dark-300 bg-white/5 p-1.5 text-dark-600"
+      className="flex items-center rounded-full border border-dark-300 bg-white/5 p-1.5 text-dark-600"
     >
-      <MagnifyingGlassIcon className="ml-5 size-6 shrink-0" />
+      <MagnifyingGlassIcon className="ml-5 mr-4 size-6 shrink-0" />
       <p className="mr-auto">企画を検索する</p>
       <Link
         to="/map/booth"
         className="flex items-center gap-2 rounded-full bg-primary-100 px-6 py-2 text-white"
       >
         <ShoppingBagIcon className="size-6" />
-        <p className="font-medium">模擬店</p>
+        <p className="font-medium max-[370px]:hidden">模擬店</p>
       </Link>
     </Link>
   )
